01ac42ba01e15269495df4c97a24496517320831
Antonio Sanchez <antonio.buap@gmail.com>
2018-10-13 17:38:12 -0500

    Add I2C communication protocol with the temperature sensor.

--- test/testHIH6130.cpp
+++ test/testHIH6130.cpp
@@ -17,32 +17,45 @@ using namespace std;
 // Small macro to display value in hexadecimal with 2 places
 #define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
 #define DEVID       0x00
-#define BUFFER_SIZE 40
-#define SENSOR_ADDR 0x27
+#define BUFFER_SIZE 3
 
-int main(){
-   int file;
-   cout << "Starting the HIH-6130 test application" << endl;
-   if((file=open("/dev/i2c-1", O_RDWR)) < 0){
-      cout << "Failed to open the I2C Bus" << endl;
-      return 1;
-   }
-   if(ioctl(file, I2C_SLAVE, SENSOR_ADDR) < 0){
-      cout << "Failed to connect to the sensor" << endl;
-      return 1;
-   }
-   /*char writeBuffer[1] = {0x00};
-   if(write(file, writeBuffer, 1)!=1){
-      cout << "Failed to reset the read address" << endl;
-      return 1;
-   }
-   char readBuffer[BUFFER_SIZE];
-   if(read(file, readBuffer, BUFFER_SIZE)!=BUFFER_SIZE){
-      cout << "Failed to read in the buffer" << endl;
-      return 1;
-   }
-   cout << "The Device ID is: " << HEX(readBuffer[DEVID]) << endl;
-   */
-   close(file);
-   return 0;
+const char HIH_SLAVE_ADDR = 0x27U;
+
+int main()
+{
+    int file;
+    cout << "Starting the HIH-6130 test application" << endl;
+    if((file = open("/dev/i2c-1", O_RDWR)) < 0)
+    {
+        cout << "Failed to open the I2C Bus" << endl;
+        return 1;
+    }
+
+    if(ioctl(file, I2C_SLAVE, HIH_SLAVE_ADDR) < 0)
+    {
+        cout << "Failed to connect to the sensor" << endl;
+        return 1;
+    }
+    
+    // 1. Humidity and Temperature Measurement Request
+    char writeBuffer[1] = { HIH_SLAVE_ADDR << 1 };
+    if( write(file, writeBuffer, 1) != 1 )
+    {
+        cout << "Failed to write the Measurement Request (MR) command" << endl;
+        return 1;
+    }
+
+    // 2. Humidity and Temperature Data Fetch
+    char readBuffer[BUFFER_SIZE];
+    if( read(file, readBuffer, BUFFER_SIZE) != BUFFER_SIZE)
+    {
+        cout << "Failed to read in the buffer" << endl;
+        return 1;
+    }
+    
+    cout << "The device status is " << HEX(readBuffer[DEVID] >> 6) << endl;
+
+    close(file);
+    
+    return 0;
 }

5780afc4cd3f02dc8db427f167c9d01a8727dfa1
Antonio Sanchez <antonio.buap@gmail.com>
2018-10-16 21:57:03 -0500

    Trigger a measurement request with a new byte. Add small delay. Add a switch-case statement for diagnostic states.

--- test/testHIH6130.cpp
+++ test/testHIH6130.cpp
@@ -17,7 +17,7 @@ using namespace std;
 // Small macro to display value in hexadecimal with 2 places
 #define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
 #define DEVID       0x00
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 4
 
 const char HIH_SLAVE_ADDR = 0x27U;
 
@@ -25,7 +25,7 @@ int main()
 {
     int file;
     cout << "Starting the HIH-6130 test application" << endl;
-    if((file = open("/dev/i2c-1", O_RDWR)) < 0)
+    if((file = open("/dev/i2c-2", O_RDWR)) < 0)
     {
         cout << "Failed to open the I2C Bus" << endl;
         return 1;
@@ -38,22 +38,43 @@ int main()
     }
     
     // 1. Humidity and Temperature Measurement Request
-    char writeBuffer[1] = { HIH_SLAVE_ADDR << 1 };
+    char writeBuffer[1] = { 0xFF };
     if( write(file, writeBuffer, 1) != 1 )
     {
         cout << "Failed to write the Measurement Request (MR) command" << endl;
         return 1;
     }
 
+    sleep(1);
+
     // 2. Humidity and Temperature Data Fetch
-    char readBuffer[BUFFER_SIZE];
+    char readBuffer[BUFFER_SIZE] = { 0x00 };
     if( read(file, readBuffer, BUFFER_SIZE) != BUFFER_SIZE)
     {
         cout << "Failed to read in the buffer" << endl;
         return 1;
     }
     
-    cout << "The device status is " << HEX(readBuffer[DEVID] >> 6) << endl;
+    // Diagnostic states.
+    char state = readBuffer[0] >> 6;
+    switch(state)
+    {
+        case 0:
+            printf("Diagnostic State: Normal operation\n");
+            break;
+        case 1:
+            printf("Diagnostic State: Stale data\n");
+            break;
+        case 2:
+            printf("Diagnostic State: Command mode\n");
+            break;
+        case 3:
+            printf("Diagnostic State: Diagnostic condition\n");
+            break;
+        default:
+            printf("Diagnostic State: Invalid operation\n");
+            break;
+    }
 
     close(file);
     

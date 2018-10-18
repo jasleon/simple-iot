465295b5ab51f243e1840b5f45a185d885c11f9e
Antonio Sanchez <antonio.buap@gmail.com>
2018-10-17 20:58:23 -0500

    Calculate the humidity, temperature and diagnostic state from the HIH-6130 message. Add a for-loop to take multiple measurements.

--- test/testHIH6130.cpp
+++ test/testHIH6130.cpp
@@ -12,6 +12,7 @@
 #include<linux/i2c-dev.h>
 #include<iomanip>
 #include<unistd.h>
+#include <string>
 using namespace std;
 
 // Small macro to display value in hexadecimal with 2 places
@@ -21,8 +22,47 @@ using namespace std;
 
 const char HIH_SLAVE_ADDR = 0x27U;
 
+static string getDiagnostic(unsigned char dataByte[4])
+{
+    string message;
+    switch(dataByte[0] >> 6)
+    {
+        case 0:
+            message.assign("Normal operation");
+            break;
+        case 1:
+            message.assign("Stale data");
+            break;
+        case 2:
+            message.assign("Command mode");
+            break;
+        case 3:
+            message.assign("Diagnostic condition");
+            break;
+        default:
+            message.assign("Invalid operation");
+            break;
+    }
+    return message;
+}
+
+static double getTemperature(unsigned char dataByte[4])
+{
+    unsigned int output_count = (dataByte[2] << 6) | (dataByte[3] >> 2);
+    double temperature = (static_cast<double>(output_count) / 16383.0) * 165.0 - 40.0;
+    return temperature;
+}
+
+static double getHumidity(unsigned char dataByte[4])
+{
+    unsigned int output_count = ((dataByte[0] & ~0xC0) << 8) | dataByte[1];
+    double humidity = (static_cast<double>(output_count) / 16383.0) * 100.0;
+    return humidity;
+}
+
 int main()
 {
+    // 1. Configure I2C communication with HIH-6130.
     int file;
     cout << "Starting the HIH-6130 test application" << endl;
     if((file = open("/dev/i2c-2", O_RDWR)) < 0)
@@ -37,45 +77,33 @@ int main()
         return 1;
     }
     
-    // 1. Humidity and Temperature Measurement Request
-    char writeBuffer[1] = { 0xFF };
-    if( write(file, writeBuffer, 1) != 1 )
-    {
-        cout << "Failed to write the Measurement Request (MR) command" << endl;
-        return 1;
-    }
+    unsigned char writeBuffer[1] = { 0xFF };
+    unsigned char readBuffer[BUFFER_SIZE] = { 0x00 };
 
-    sleep(1);
-
-    // 2. Humidity and Temperature Data Fetch
-    char readBuffer[BUFFER_SIZE] = { 0x00 };
-    if( read(file, readBuffer, BUFFER_SIZE) != BUFFER_SIZE)
+    for(size_t k = 1U; k < 6; k++)
     {
-        cout << "Failed to read in the buffer" << endl;
-        return 1;
+        cout << "===== Measurement #" << k << " =====" << endl; 
+        // 2. Send a measurement request to the sensor.
+        if( write(file, writeBuffer, 1) != 1 )
+        {
+            cout << "Failed to write the Measurement Request (MR) command" << endl;
+            return 1;
+        }
+
+        sleep(1);
+
+        // 3. Fetch humidity and temperature data from the sensor.
+        if( read(file, readBuffer, BUFFER_SIZE) != BUFFER_SIZE)
+        {
+            cout << "Failed to read in the buffer" << endl;
+            return 1;
+        }
+        
+        cout << "Diagnostic:\t"  << getDiagnostic(readBuffer)            << endl;
+        cout << "Temperature:\t" << getTemperature(readBuffer) << " °C"  << endl;
+        cout << "Humidity:\t"    << getHumidity(readBuffer)    << " %RH" << endl;
     }
     
-    // Diagnostic states.
-    char state = readBuffer[0] >> 6;
-    switch(state)
-    {
-        case 0:
-            printf("Diagnostic State: Normal operation\n");
-            break;
-        case 1:
-            printf("Diagnostic State: Stale data\n");
-            break;
-        case 2:
-            printf("Diagnostic State: Command mode\n");
-            break;
-        case 3:
-            printf("Diagnostic State: Diagnostic condition\n");
-            break;
-        default:
-            printf("Diagnostic State: Invalid operation\n");
-            break;
-    }
-
     close(file);
     
     return 0;

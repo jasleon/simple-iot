db0284344586ebc7552837ffd7ab570b300db131
jasleon <antonio.buap@gmail.com>
2020-08-07 19:58:31 -0500

    Add more bugfixes, and complete constructor

--- HIH6130.cpp
+++ HIH6130.cpp
@@ -5,15 +5,37 @@
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 
 using namespace std;
 
-HIH6130::HIH6130(const string device) : fd(-1), is_open(false), temperature(0.0f), humidity(0.0f)
+HIH6130::HIH6130(string device, unsigned int address = 0x27U) : 
+    fd(-1), 
+    is_open(false),
+    is_connected(false), 
+    temperature(0.0f), 
+    humidity(0.0f)
 {
     fd = open(device.c_str(), O_RDWR);
-    is_open = fd < 0;
+    is_open = fd >= 0;
     
     if(is_open)
+    {
+        cout << "File descriptor: " << fd << endl;
+        is_connected = ioctl(fd, I2C_SLAVE, address) >= 0;
+        
+        if(is_connected)
+        {
+            cout << "Sensor connected" << endl;
+        }
+        else
+        {
+            cout << "Failed to connect to the sensor" << endl;
+        }
+    }
+    else
     {
         cout << "Failed to open the I2C Bus" << endl;
     }
@@ -24,6 +46,8 @@ HIH6130::~HIH6130()
     if(is_open)
     {
         close(fd);
+
+        cout << "Closed the file descriptor" << endl;
     }
 }
 
--- HIH6130.hpp
+++ HIH6130.hpp
@@ -10,11 +10,12 @@ class HIH6130
     private:
         int fd;
         bool is_open;
+        bool is_connected;
         double temperature;
         double humidity;
     
     public:
-        HIH6130(const string device);
+        HIH6130(string device, unsigned int address);
         ~HIH6130();
         double getTemperature() const;
         double getHumity() const;
